417a2b2cc8a74f78d72855ca794b436782849209
yibeltalawok <134057013+yibeltalawok@users.noreply.github.com>
2024-06-27 11:32:39 +0300

    Delete fileuploadBackEnd directory

--- fileuploadBackEnd/routes/likes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var likes = require('../controller/likes');
-// router.post('/likes',likes.create);  
-router.put("/:id", likes.update);
-router.get('/all', likes.findAll);
-//  router.get('/:id',likes.fetchOne);
-module.exports = router;
-
